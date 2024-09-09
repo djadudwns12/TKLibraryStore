9d452a7dd9f44b96251c5a37fa0ee7500acc260d
pgyseven <pgyseven@naver.com>
2024-09-09 18:01:48 +0900

    feat: coolsms 구현중

--- src/main/java/com/tn/member/controller/MemberController.java
+++ src/main/java/com/tn/member/controller/MemberController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import com.miniproj.controller.coolsms.ValidationUtil;
+
 
 import net.nurigo.sdk.NurigoApp;
 import net.nurigo.sdk.message.model.Message;
@@ -44,46 +44,45 @@ public class MemberController {
 	
 //////////////////////////////////박근영///////////////////////////////////////
 	
-	@RequestMapping("/coolsms")
-	public class CoolsmsController {
-		   final DefaultMessageService messageService;
-
-		    public 
-		    CoolsmsController() {
-		        // 반드시 계정 내 등록된 유효한 API 키, API Secret Key를 입력해주셔야 합니다!
-		        this.messageService = NurigoApp.INSTANCE.initialize("NCSIFSCSZASSG9F9", "UZ8YAMBTCSGFQUVBFEPPDFCWHAOZRZDP", "https://api.coolsms.co.kr");
-		    }
-
-		    @PostMapping("/send-one")
-		    public SingleMessageSentResponse sendOne(@RequestParam("mobile") String mobile) {
-		        Message message = new Message();
-		        
-		        message.setFrom("010-3888-9567");
-		        message.setTo(mobile);
-		        ValidationUtil validationUtil = new ValidationUtil();
-		        int code = validationUtil.createCode();
-		        message.setText("인증번호는(" + code + ")입니다. 정확히 입력해 주세요.");
-
-		        SingleMessageSentResponse response = this.messageService.sendOne(new SingleMessageSendingRequest(message));
-		        System.out.println(response);
-
-		        return response;
-		    }
-
-
-		}
-
-	class ValidationUtil {
-
-	    // 인증 코드를 생성하는 메소드
-	    public int createCode() {
-	        // 간단한 예시로 6자리 숫자 코드 생성
-	        //int code = (int) (Math.random() * 900000) + 100000;
-	        Random random = new Random();
-	        int code = 100000 + random.nextInt(900000);
-	        return code;
-	    }
-	}
-	
+//	@RequestMapping("/coolsms")
+//	public String CoolsmsController {
+//		   final DefaultMessageService messageService;
+//
+//		    public void CoolsmsController() {
+//		        // 반드시 계정 내 등록된 유효한 API 키, API Secret Key를 입력해주셔야 합니다!
+//		        this.messageService = NurigoApp.INSTANCE.initialize("NCSIFSCSZASSG9F9", "UZ8YAMBTCSGFQUVBFEPPDFCWHAOZRZDP", "https://api.coolsms.co.kr");
+//		    }
+//
+//		    @PostMapping("/send-one")
+//		    public SingleMessageSentResponse sendOne(@RequestParam("mobile") String mobile) {
+//		        Message message = new Message();
+//		        
+//		        message.setFrom("010-3888-9567");
+//		        message.setTo(mobile);
+//		        ValidationUtil validationUtil = new ValidationUtil();
+//		        int code = validationUtil.createCode();
+//		        message.setText("인증번호는(" + code + ")입니다. 정확히 입력해 주세요.");
+//
+//		        SingleMessageSentResponse response = this.messageService.sendOne(new SingleMessageSendingRequest(message));
+//		        System.out.println(response);
+//
+//		        return response;
+//		    }
+//
+//
+//		}
+//
+//	class ValidationUtil {
+//
+//	    // 인증 코드를 생성하는 메소드
+//	    public int createCode() {
+//	        // 간단한 예시로 6자리 숫자 코드 생성
+//	        //int code = (int) (Math.random() * 900000) + 100000;
+//	        Random random = new Random();
+//	        int code = 100000 + random.nextInt(900000);
+//	        return code;
+//	    }
+//	}
+//	
 	
 }
