373c524af62ae76398bf955e3c025afab1c9ae6c
djadudwns12 <djadudwns12@naver.com>
2024-09-20 19:22:30 +0900

    feat : 로그인 여부를 체크하기위한 AuthInterceptor 생성

---
+++ src/main/java/com/tn/interceptor/AuthInterceptor.java
@@ -0,0 +1,44 @@
+package com.tn.interceptor;
+
+import javax.mail.Session;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.ui.Model;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class AuthInterceptor extends HandlerInterceptorAdapter {
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		// TODO Auto-generated method stub
+		System.out.println("auth 인터셉터 preHandle");
+		
+		boolean goController = false;
+		
+		HttpSession sess = request.getSession();
+		
+		if(sess.getAttribute("loginMember")==null) {
+			response.sendRedirect("/member/loginPage");
+		}else {
+			goController = true;
+		}
+		
+		
+		return goController;
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+			ModelAndView modelAndView) throws Exception {
+		// TODO Auto-generated method stub
+		super.postHandle(request, response, handler, modelAndView);
+	}
+	
+
+	
+}
