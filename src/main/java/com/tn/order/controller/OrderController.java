e5aeb5f6af85e5c4a1ab235e5c6587256a6a23cb
gayoon-create <kgykgy1123@naver.com>
2024-10-01 14:56:48 +0900

    feat : 보내지는지 확인하려고 컨트롤러 일단 해봤음

--- src/main/java/com/tn/order/controller/OrderController.java
+++ src/main/java/com/tn/order/controller/OrderController.java
@@ -1,39 +1,23 @@
 package com.tn.order.controller;
 
-import java.text.DateFormat;
-import java.util.Date;
-import java.util.Locale;
+import java.util.Map;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
-/**
- * Handles requests for the application home page.
- */
+import com.tn.cart.model.dto.OrderInfoDTO;
+
 @Controller
+@RequestMapping("/order")
 public class OrderController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(OrderController.class);
-	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		return "home";
+	@PostMapping("/payment")
+	public ResponseEntity<Map<String, Boolean>> processPayment(@RequestBody OrderInfoDTO orderInfoDTO) {
+	    // 결제 처리 로직
+	    return ResponseEntity.ok().body(Map.of("success", true));
 	}
 	
 }
