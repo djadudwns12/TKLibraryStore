a3d8fd7e176c86333b28cb11b59dd183bb513762
djadudwns12 <djadudwns12@naver.com>
2024-10-02 10:31:04 +0900

    fix : 에러 수정

--- src/main/java/com/tn/interceptor/AuthInterceptor.java
+++ src/main/java/com/tn/interceptor/AuthInterceptor.java
@@ -3,6 +3,7 @@ package com.tn.interceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import com.tn.interceptor.DestiationPath;
 
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
--- src/main/java/com/tn/order/controller/OrderController.java
+++ src/main/java/com/tn/order/controller/OrderController.java
@@ -2,6 +2,8 @@ package com.tn.order.controller;
 
 import java.util.Map;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
