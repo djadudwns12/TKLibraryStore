96666a48165f781b0019f301c77ff3ed83a5d2ad
pgyseven <pgyseven@naver.com>
2024-10-10 11:56:27 +0900

    fix 수정

--- src/main/java/com/tn/order/dao/OrderDAOImpl.java
+++ src/main/java/com/tn/order/dao/OrderDAOImpl.java
@@ -22,7 +22,7 @@ public class OrderDAOImpl implements OrderDAO {
 	private SqlSession ses;
 
 	
-	private String NS = "com.tn.mapper.orderMapper";
+	private String NS = "com.tn.mapper.OrderMapper";
 	
 	
 //-----------------------------------------박근영-------------------------------------------------
