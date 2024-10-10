e83eb5d5308673af60b7c8548b3152ff4dd650c8
ahrimbau <ahrimbau@gmail.com>
2024-10-10 09:40:30 +0900

    feat: 별점 및 리뷰 기능 구현중

--- src/main/java/com/tn/review/model/DTO/ReviewDTO.java
+++ src/main/java/com/tn/review/model/DTO/ReviewDTO.java
@@ -1,5 +1,14 @@
 package com.tn.review.model.DTO;
 
+import java.sql.Date;
+
 public class ReviewDTO {
+	private int reviewNo;
+	private String reviewWriter;
+	private String reviewContent;
+	private Date reviewDate;
+	private int reviewScore;
+	private int bookNo;
+	
 
 }
