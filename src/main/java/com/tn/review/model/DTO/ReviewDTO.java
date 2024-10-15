3fc0dc007d1ed287b3d68c5ddc334283ff0867ca
gayoon-create <kgykgy1123@naver.com>
2024-10-15 18:13:29 +0900

    feat : 어노테이션 추가

--- src/main/java/com/tn/review/model/DTO/ReviewDTO.java
+++ src/main/java/com/tn/review/model/DTO/ReviewDTO.java
@@ -2,6 +2,20 @@ package com.tn.review.model.DTO;
 
 import java.sql.Date;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Setter
+@ToString
+@Builder
 public class ReviewDTO {
 	private int reviewNo;
 	private String reviewWriter;
